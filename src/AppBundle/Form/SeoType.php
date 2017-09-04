2bd1362f6a95e5d808bae6f56dd3d32be22779be
manolo <emmanuelripoll@gmail.com>
2017-09-04 10:59:57 +0200

    Create model

---
+++ src/AppBundle/Form/SeoType.php
@@ -0,0 +1,25 @@
+<?php
+
+
+namespace AppBundle\Form;
+
+
+use AppBundle\Entity\Seo;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SeoType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title')
+            ->add('description');
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(['data_class' => Seo::class]);
+    }
+}
